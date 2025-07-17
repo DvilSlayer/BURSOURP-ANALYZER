754d535b8fd69544081f93ffdc3fa7042450bd30
DvilSlayer <141831134+DvilSlayer@users.noreply.github.com>
2025-07-17 11:33:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import pandas as pd
 import openpyxl
 dp = pd.read_excel('BUSROP_Students_questionnaire_all_versions_labels_2024_04_08_19.xlsx')
